53319cccf108eed670b7d062e74df0ab2dfda560
Nick <nick@wsip-184-177-172-90.sd.sd.cox.net>
2017-02-08 10:49:22 -0800

    adding jade templating

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-console.log(process); 
+
 
 //static file directory
 app.use(express.static('public'));
-app.use(express.static('src/views'));
-
+app.set('views','./src/views');
+app.set('view engine', 'jade');
 
 
 app.get('/', function(req, res){
-    res.send('Jello World');
+    res.render('index', {list:['a','b']});
 });
 
 app.get('/books', function(req, res){
