55f46ce1a5de5b2b417d58a5d1a16370c6d7b804
Nick <nick@wsip-184-177-172-90.sd.sd.cox.net>
2017-02-09 10:52:10 -0800

    updating gulp file with comments and support to inject src files to index.

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
+//init express
 var express = require ('express');
-
 var app = express();
 
+//use port 5000 or use port designated in nodemon
 var port = process.env.PORT || 5000;
 
 
 
 //static file directory
 app.use(express.static('public'));
-app.set('views','./src/views');
 
+//Config
+app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
-
+//Routing
 app.get('/', function(req, res){
     res.render('index', {title: 'Hello from render', list:['a','b']});
 });
-
 app.get('/books', function(req, res){
     res.send('Jello books');
 });
 
-app.listen(5000, function(err){
+//Server 
+app.listen(port, function(err){
     console.log('running server on port ' + port);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var nodemon = require('gulp-nodemon');
 
+//The JS files to watch
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 
 
 //STYLE
+//checks all js for code standards
 gulp.task('style', function(){
     gulp.src(jsFiles)
     .pipe(jshint())
@@ -18,34 +20,40 @@ gulp.task('style', function(){
 });
 
 //INJECT DEP
+//injectiong script and style files to index.ejs
 gulp.task('inject', function(){
     var wiredep = require('wiredep').stream;
     var inject = require('gulp-inject');
 
+    //inject options
     var injectSrc = gulp.src(['./public/css/*.css',
                                './public/js/*.js' ], {read:false});
-
     var injectOptions = {
         ignorePath: '/public'
     };
 
+    //wiredep optiomns
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
     };
-    return gulp.src('./src/views/*.jade')
+
+    return gulp.src('./src/views/*.ejs')
     .pipe(wiredep(options))
     .pipe(inject(injectSrc, injectOptions))
     .pipe(gulp.dest('./src/views'));
 });
 
+//The Server
 gulp.task('serve', ['style', 'inject'], function(){
+
+    //nodemon options
     var options = {
         script: 'app.js',
         delayTime: 1,
         env: {
-            'PORT': 5000
+            'PORT': 3000
         },
         watch: jsFiles
     };
